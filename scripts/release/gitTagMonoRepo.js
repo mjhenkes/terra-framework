ba26e291aa80f19d6df561e7fac8e74e36ff7d73
Matt Henkes <matt.henkes@cerner.com>
2020-07-17 00:19:40 -0500

    setup git to allow pushing tags

--- scripts/release/gitTagMonoRepo.js
+++ scripts/release/gitTagMonoRepo.js
@@ -1,6 +1,19 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
 
+const setupGit = () => {
+  const travis = process.env.TRAVIS;
+
+  if (travis) {
+    execSync('git config --global user.email "travis@travis-ci.org"');
+    execSync('git config --global user.name "Travis CI"');
+    const remoteUrl = execSync('git config --get remote.origin.url');
+    const token = process.env.GITHUB_TOKEN;
+    const newUrl = remoteUrl.replace('https://', `https://${token}@`);
+    execSync(`git remote set-url origin "${newUrl}" > /dev/null 2>&1`);
+  }
+};
+
 const getTags = (output) => {
   const successString = 'Successfully published:\n';
   const lastIndexOf = output.lastIndexOf(successString);
@@ -19,6 +32,7 @@ if (fs.existsSync('./publish-output.txt')) {
   console.log(output);
   const tags = getTags(output);
   if (tags) {
+    setupGit();
     console.log('tags', JSON.stringify(tags, null, 2));
     tags.forEach(tag => execSync(`git tag -a ${tag} -m "${tag}"`));
     execSync('git push origin --tags');
